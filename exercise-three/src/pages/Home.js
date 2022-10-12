e76189894fc6209bf86bd3251ad6504f22b69e2a
Paloma St.Clair <palomast.clair@10-18-18-111.dynapool.wireless.nyu.edu>
2022-10-12 16:20:37 -0400

    weather data working

--- exercise-three/src/pages/Home.js
+++ exercise-three/src/pages/Home.js
@@ -1,26 +1,29 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { WEATHER_APP_API_KEY } from "../API_KEYS";
 
 function Home() {
-    const openWeatherURL = ``;
-    // const [weatherData, setWeatherDta] = useState({});
+    const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${WEATHER_APP_API_KEY}`;
+    const [weatherData, setWeatherData] = useState({});
     useEffect(() => {
         axios
             .get(openWeatherURL)
             .then( function (response) {
-                console.log(response);
+                setWeatherData(response.data);
             })
             .catch(function(error){
                 console.warn(error);
+                setWeatherData({});
             });
         // display this weather data on our app 
        
     }, []); 
+    console.log("state value", weatherData);
     return (
         <div>
             <h1>Weather App</h1>
-            <p>{WEATHER_APP_API_KEY}</p>
+            <h2>{weatherData.name}</h2>
+            <p>Humidity: {weatherData.main && weatherData.main.humidity} </p>
         </div>
     );
 }
