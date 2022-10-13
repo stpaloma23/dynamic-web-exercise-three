76c3d400dc96b83a1c55973168602dae8ce2e883
Paloma St.Clair <palomast.clair@10-21-227-89.dynapool.wireless.nyu.edu>
2022-10-13 00:57:16 -0400

    css added, but temperatures are wrong

--- exercise-three/src/components/Header.js
+++ exercise-three/src/components/Header.js
@@ -3,11 +3,14 @@ import React from "react";
 function Header () {
     return (
         <header>
-            <a href="/?city=Bali">Bali</a>
-            <a href="/?city=Cairo">Cairo</a>
-            <a href="/?city=Paris">Paris</a>
-            <a href="/?city=Johannesburg">Johannesburg</a>
-            <a href="/?city=Seattle">Seattle</a>
+            <h1 className="weather-app-title">World Weather App</h1>
+            <div className="header-links">
+                <a href="/?city=Bali">Bali</a>
+                <a href="/?city=Cairo">Cairo</a>
+                <a href="/?city=Paris">Paris</a>
+                <a href="/?city=Johannesburg">Johannesburg</a>
+                <a href="/?city=Seattle">Seattle</a>
+            </div>
         </header>
     )
 }
