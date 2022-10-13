896b9f3901727d3dc340542ae4b3abd64b834503
Paloma St.Clair <palomast.clair@10-18-18-111.dynapool.wireless.nyu.edu>
2022-10-13 09:28:44 -0400

    more cases to switch statement

--- exercise-three/src/components/WeatherIcon.js
+++ exercise-three/src/components/WeatherIcon.js
@@ -5,14 +5,17 @@ import {faCloud, faSun, faUmbrella, faSnowflake, faBolt} from "@fortawesome/free
 function WeatherIcon({weatherType}) {
     switch (weatherType) {
         case 'Clouds':
+        case 'Fog':
             return <FontAwesomeIcon icon={faCloud}/>;
         case 'Clear':
+        case 'Sun':
             return <FontAwesomeIcon icon={faSun}/>;
         case 'Rain':
+        case 'Drizzle':
             return <FontAwesomeIcon icon={faUmbrella}/>;
         case 'Snow':
             return <FontAwesomeIcon icon={faSnowflake}/>;
-        case "Thunder":
+        case "Thunderstorm":
             return <FontAwesomeIcon icon={faBolt}/>;
         default:
             return null;
